9c4da6e7506f3aa981c4c15793f14dfe86ac7184
alupoaiemircea <mircea@axiologic.net>
2024-01-16 13:58:37 +0200

    update flows

--- Chatbots#56siYYa2GpTS.js
+++ Chatbots#56siYYa2GpTS.js
@@ -5,9 +5,16 @@ export class Chatbots {
 
     }
 
-    start(prompt, personalityId, replyHistory) {
+    async start(chatbot, conversation, prompt, emotion, personalityId, replyHistory) {
         let personality = webSkel.currentUser.space.getPersonality(personalityId);
-        this.prompt = `Step into the shoes of ${personality.name}, a character known for their distinctive traits: ${personality.description}. Your mission is to respond to the given text: "${prompt}" while emanating an emotion that naturally aligns with the text and encapsulates the distinct essence of this character. Please answer in the same language as the given text. Please only use emojis that have faces. The response should have the following structure: {\"reply\": \"reply to the given text\", \"emotion\":{\"name\":\"name of the emotion\", \"emoji\":\"emoji of the emotion\"}}`;
+        let systemMessage = `Step into the shoes of ${personality.name}, a character known for their distinctive traits: ${personality.description}. Your mission is to respond to the replies of the user while emanating an emotion that naturally aligns with the replies and encapsulates the distinct essence of this character. Please answer in the same language as the given replies. Please only use emojis that have faces. The response should have the following structure: {\"reply\": \"reply to the given text\", \"emotion\":{\"name\":\"name of the emotion\", \"emoji\":\"emoji of the emotion\"}}`;
+        this.prompt = prompt;
+        if(!conversation.isInstructed){
+            await chatbot.addMessage(conversation, "system", systemMessage);
+            await chatbot.addContext(conversation, systemMessage);
+        }
+        conversation.setCurrentEmotion(emotion);
+        await chatbot.addMessage(conversation, "user", prompt);
         this.setDefaultValues();
         this.setIntelligenceLevel(3);
         this.setResponseFormat("json_object");
--- SummarizeConversation#2DUFX4A3fmPv.js
+++ SummarizeConversation#2DUFX4A3fmPv.js
@@ -6,20 +6,15 @@ export class SummarizeConversation {
     }
 
     start(replyHistory) {
-        this.prompt = `Please summarize the following conversation: "${replyHistory}" by creating the main idea that each role has. the response should have the following structure: {\"summary\":[{role:\"user\", content:\"main idea of user replies\"}, {role:\"assistant\", content:\"main idea of assistant replies\"}]}.`;
-        this.setThink("");
+        this.prompt = `Please summarize the following conversation between a user and an AI model by extracting information about the discussion: "${JSON.stringify(replyHistory)}". Keep in mind the subject they are discussing and the questions and answers that have been said. The AI model is using a predefined personality in order to give an authentic chatting experience.`;
+        this.setThink("Thinking...");
         this.setDefaultValues();
         this.setIntelligenceLevel(3);
         this.execute();
     }
 
     async execute() {
-        let summary = await this.summarize(this.prompt);
-        try {
-            JSON.parse(summary);
-        } catch (e) {
-            this.fail(e);
-        }
+        let summary = await this.request(this.prompt);
         this.return(summary);
     }
 }
